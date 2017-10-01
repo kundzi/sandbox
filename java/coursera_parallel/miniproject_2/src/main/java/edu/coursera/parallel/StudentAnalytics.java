ee00e84cf44909739cd24f87eb7951778a2ea3aa
Dima Kunin <mr.kunin@gmail.com>
2017-10-01 20:34:15 +0200

    another problem solved

--- java/coursera_parallel/miniproject_2/src/main/java/edu/coursera/parallel/StudentAnalytics.java
+++ java/coursera_parallel/miniproject_2/src/main/java/edu/coursera/parallel/StudentAnalytics.java
@@ -1,9 +1,14 @@
 package edu.coursera.parallel;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -105,7 +110,20 @@ public final class StudentAnalytics {
      */
     public String mostCommonFirstNameOfInactiveStudentsParallelStream(
             final Student[] studentArray) {
-        throw new UnsupportedOperationException();
+        return Stream.of(studentArray)
+            .parallel()
+            .filter(s -> !s.checkIsCurrent())
+            .map(Student::getFirstName)
+            .collect(Collectors.toMap(
+                Function.identity(),
+                name -> 1,
+                Integer::sum
+            ))
+            .entrySet()
+            .parallelStream()
+            .max(Comparator.comparingInt(Map.Entry::getValue))
+            .get()
+            .getKey();
     }
 
     /**
