ad81b799850cf208617c00e2eef71053b6f39fae
Dima Kunin <mr.kunin@gmail.com>
2020-06-26 15:17:42 +0200

    flutter: implement zooming on a map marker

--- flutter/google_maps_in_flutter/lib/main.dart
+++ flutter/google_maps_in_flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/foundation.dart';
 
 import 'locations.dart' as locations;
 
@@ -11,12 +12,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   final Map<String, Marker> _markers = {};
-
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  GoogleMapController controller;
 
   Future<void> _onMapCreated(GoogleMapController controller) async {
+    this.controller = controller;
+
     final googleOffices = await locations.getGoogleOffices();
     setState(() {
       _markers.clear();
@@ -27,8 +28,7 @@ class _MyAppState extends State<MyApp> {
             infoWindow: InfoWindow(
               title: office.name,
               snippet: office.address,
-            )
-        );
+            ));
         _markers[office.name] = marker;
       }
     });
@@ -36,19 +36,55 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    final orderedMarkers = _markers.values.toList();
+    orderedMarkers.sort((a, b) => a.markerId.value.compareTo(b.markerId.value));
+
+    var googleMap = GoogleMap(
+      myLocationEnabled: false,
+      myLocationButtonEnabled: false,
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        target: const LatLng(0, 0),
+        zoom: 2,
+      ),
+      markers: _markers.values.toSet(),
+    );
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Google Office Locations'),
-          backgroundColor: Colors.green[700], 
+          backgroundColor: Colors.green[700],
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: const LatLng(0, 0),
-            zoom: 2,
-          ),
-          markers: _markers.values.toSet(),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: googleMap,
+            ),
+            Expanded(
+              flex: 1,
+              child: ListView.separated(
+                padding: const EdgeInsets.all(8),
+                itemCount: orderedMarkers.length,
+                itemBuilder: (context, index) {
+                  var marker = orderedMarkers[index];
+                  return ListTile(
+                    title: Text(marker.infoWindow.title),
+                    subtitle: Text(marker.infoWindow.snippet),
+                    onTap: () {
+                      controller
+                          .animateCamera(
+                              CameraUpdate.newLatLngZoom(marker.position, 6))
+                          .then((value) => debugPrint('done animating'));
+                    },
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+              ),
+            )
+          ],
         ),
       ),
     );
